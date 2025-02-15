3009a34ac0f5367ef6a30bdf1f788f1c9b5da521
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2025-02-15 14:07:55 +0530

    Update schemas.py

---
+++ backend/app/schemas.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Product model.
+    """
+    class Meta:
+        model = Product
+        fields = "__all__"
